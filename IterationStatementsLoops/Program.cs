4346e07d0d6c67df60e18983b5788ab90a41306c
Bdotgo17 <bdotgo@hotmail.com>
2022-09-15 17:55:18 -0500

    Initial Commit

---
+++ IterationStatementsLoops/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IterationStatementsLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            for (int i = 0; i <= 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
+
